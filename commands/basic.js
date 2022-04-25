2a2b0bd29bf3350931d8c3f5e123f447ee4ad88b
NFSS10 <nuno_nfss96@hotmail.com>
2022-04-25 20:52:19 +0100

    Add leave command

---
+++ commands/basic.js
@@ -0,0 +1,32 @@
+const { getVoiceConnection } = require("@discordjs/voice");
+
+const runCommand = async (client, interaction) => {
+    const commandName = interaction.commandName;
+    switch (commandName) {
+        case "leave":
+            await leave(client, interaction);
+            break;
+        default:
+            await interaction.reply("Invalid command");
+    }
+};
+
+const leave = async (client, interaction) => {
+    const connection = getVoiceConnection(interaction.guildId);
+    await connection?.destroy();
+    await interaction.reply("Bye! :)");
+};
+
+const cleanup = async () => {};
+
+module.exports = {
+    name: "about",
+    runCommand: runCommand,
+    cleanup: cleanup,
+    commands: [
+        {
+            name: "leave",
+            description: "Disconnects from the voice channel"
+        }
+    ]
+};
